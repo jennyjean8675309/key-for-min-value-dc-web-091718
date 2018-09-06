82ef3fa40c9f27a3f577a05524c2c8f89b15a717
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-06 15:20:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,9 +20,6 @@ def key_for_min_value(name_hash)
         sample_numbers.shift
       elsif sample_numbers[i] < number 
         sample_numbers.delete(number)
-          if sample_numbers[i] < number
-            sample_numbers.delete(number)
-          end
       end
       i += 1
     end
