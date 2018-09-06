66fe6f2f3d63e0d6eba8ed7ad3d607ee1027201a
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-06 15:35:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  sample_numbers = []
+  # sample_numbers = []
   winner = ''
   if name_hash == {}
     nil
@@ -29,5 +29,5 @@ def key_for_min_value(name_hash)
       end
     end 
     winner
-  end
+  end 
 end
